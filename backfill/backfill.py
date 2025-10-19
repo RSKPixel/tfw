ab0f4aaee5c2768b5ef45218da771861aa3188fd
Saravana Kumar <saravanakumar@Saravanas-MacBook-Air.local>
2025-10-19 07:50:49 +0530

    update

---
+++ backfill/backfill.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import django
+from kiteconnect import KiteConnect
+from kiteconnect.exceptions import TokenException
+import config
+import time
+from datetime import datetime
+import requests
+from dotenv import load_dotenv
+import webbrowser
+load_dotenv()
+
+
+def main():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(BASE_DIR)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    django.setup()
+
+    from data.views import historicals
+
+    api_key = config.KITE_API_KEY
+    api_secret = config.KITE_API_SECRET
+    access_token_api_url = os.getenv("ACCESS_TOKEN_API_URL")
+
+    request = requests.get(access_token_api_url)
+    access_token = request.json().get("access_token", "")
+
+    kite = KiteConnect(api_key=api_key)
+    try:
+        kite.set_access_token(access_token)
+        profile = kite.profile()
+    except Exception as e:
+        print("Error setting access token:", e)
+        # open login URL in browser
+
+        loginurl = kite.login_url()
+        kite = None
+        print("Login URL:", loginurl)
+        webbrowser.open(loginurl)
+        return
+
+    first_run = True
+    period = 1
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        if first_run:
+            try:
+                period = int(
+                    input("Initial run - Enter number of days to backfill: 0-31: "))
+            except ValueError:
+                period = 1
+                print("Invalid input. Defaulting to 1 day.")
+
+            if period > 31:
+                print("Invalid input. Defaulting to 31 days.")
+                period = 31
+            if period < 1:
+                print("Invalid input. Defaulting to 1 day.")
+                period = 1
+
+            first_run = False
+        else:
+            period = 1
+
+        historicals(exchange='nfo', period=period, interval='minute', api=kite)
+
+        if not check_market_hours():
+            break
+
+        wait_until_next(waiting_minutes=1)
+
+
+def check_market_hours():
+    now = datetime.now()
+    start_time = datetime.strptime("09:15:00", "%H:%M:%S").time()
+    end_time = datetime.strptime("15:30:00", "%H:%M:%S").time()
+    current_time = now.time()
+
+    # If current time is outside market hours, exit loop
+    if current_time < start_time or current_time > end_time:
+        print(
+            f"\n⛔ Outside market hours ({current_time.strftime('%H:%M:%S')}). Exiting loop.")
+        return False  # indicate stop condition
+
+    return True  # indicate continue condition
+
+
+def wait_until_next(waiting_minutes=1):
+    now = datetime.now()
+    next_minute = (now.minute // waiting_minutes + 1) * waiting_minutes
+    if next_minute == 60:
+        next_run = now.replace(hour=(now.hour + 1) %
+                               24, minute=0, second=1, microsecond=0)
+    else:
+        next_run = now.replace(minute=next_minute, second=1, microsecond=0)
+
+    wait_seconds = int((next_run - now).total_seconds())
+    print(f"Next run scheduled at {next_run.strftime('%H:%M:%S')}")
+
+    try:
+        while True:
+            remaining = int((next_run - datetime.now()).total_seconds())
+            if remaining <= 0:
+                break
+            mins, secs = divmod(remaining, 60)
+            print(
+                f"\r⏳ Sleeping... {mins:02d}m {secs:02d}s remaining", end="", flush=True)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\n⛔️ Interrupted by user.")
+        exit(0)
+
+    print("\r✅ Woke up for next run!                      ", end="\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ backfill/views.py
@@ -0,0 +1,295 @@
+from django.shortcuts import render
+from django.utils import timezone
+from datetime import datetime, timedelta
+import pandas as pd
+import os
+import time
+from datetime import datetime, timedelta
+import pandas as pd
+import time
+from rich.console import Console
+from rich.table import Table
+from rich.panel import Panel
+from rich import box
+from rich.progress import Progress
+import requests
+from rich.align import Align
+from .models import iData3min, iData5min, iData15min, iData60min, iData1Day
+from django.db import transaction
+from decimal import Decimal, ROUND_HALF_UP
+
+
+console = Console()
+
+
+def historicals(exchange='NFO', segment='NFO-FUT', period=1, interval='minute', api=None):
+    if not api:
+        console.print("[bold red]Kite connection failed[/bold red]")
+        return None
+
+    # Fetch instrument list and expiry dates
+    instrument_list, current_expiry, previous_expiry = instruments(exchange)
+    # instrument_list = instrument_list[:5]
+
+    # Calculate date range based on period
+    if period == 0:
+        delta = (datetime.now().date() - (previous_expiry.date())).days - 1
+    else:
+        delta = period - 1
+
+    from_date = datetime.now().date() - timedelta(days=delta)
+    if from_date < previous_expiry.date():
+        from_date = previous_expiry.date() + timedelta(days=1)
+
+    to_date = datetime.now().date()
+
+    # Print initial info
+    print_info(exchange, segment, instrument_list, interval,
+               current_expiry, previous_expiry, from_date, to_date, no_of_days=delta+1, profile=api.profile())
+
+    # Fetch historical data using Kite API
+    complete_data = api_request(
+        api, instrument_list, from_date, to_date, interval)
+
+    if complete_data is None or complete_data.empty:
+        console.print(
+            "[bold red]No data fetched from API for the given range[/bold red]")
+        return None
+
+    # Resample data if interval is 'minute'
+    resampled_data = resample_data(complete_data, interval)
+
+    # Postgresql Storage (To be implemented)
+    store_data(resampled_data)
+
+
+def store_data(resampled_data):
+    # create or update records in the database
+    start_time = time.time()
+    console.print("\n[bold cyan]Storing data to database...[/bold cyan]")
+    for key, df in resampled_data.items():
+        if df.empty:
+            continue
+
+        df.reset_index(inplace=True)
+
+        model_map = {
+            "iData3min": iData3min,
+            "iData5min": iData5min,
+            "iData15min": iData15min,
+            "iData60min": iData60min,
+            "iData1day": iData1Day,
+        }
+
+        model = model_map.get(key)
+        if not model:
+            continue
+
+        # Convert DataFrame to model objects
+        objects = [
+            model(
+                date=row['date'],
+                symbol=row['symbol'],
+                open=Decimal(str(row['open'])).quantize(
+                    Decimal('0.01'), rounding=ROUND_HALF_UP),
+                high=Decimal(str(row['high'])).quantize(
+                    Decimal('0.01'), rounding=ROUND_HALF_UP),
+                low=Decimal(str(row['low'])).quantize(
+                    Decimal('0.01'), rounding=ROUND_HALF_UP),
+                close=Decimal(str(row['close'])).quantize(
+                    Decimal('0.01'), rounding=ROUND_HALF_UP),
+                volume=row['volume']
+            )
+            for _, row in df.iterrows()
+        ]
+
+        # Use a transaction for safety and speed
+        with transaction.atomic():
+            model.objects.bulk_create(
+                objects,
+                update_conflicts=True,  # Available in Django 4.1+
+                update_fields=['open', 'high', 'low', 'close', 'volume'],
+                unique_fields=['date', 'symbol']
+            )
+    end_time = time.time()
+    console.print(
+        f"[green]Data storage completed in {end_time - start_time:.2f}s[/green]")
+
+
+def resample_data(complete_data: pd.DataFrame, interval: str):
+    if interval == 'minute' and not complete_data.empty:
+        resampling_start = time.time()
+        console.print(
+            "\n[bold cyan]Resampling data (5m, 15m, 60m, 1d)...[/bold cyan]")
+
+        sampling = {'open': 'first', 'high': 'max',
+                    'low': 'min', 'close': 'last', 'volume': 'sum'}
+        grouped_data = complete_data.groupby('symbol')
+
+        # Filter only BANKNIFTY-I for 3min data
+        data3data = complete_data[complete_data['symbol']
+                                  == 'BANKNIFTY-I'].reset_index()
+        data3data = data3data.groupby('symbol')
+
+        data3 = data3data.resample('3min', on='date').agg(sampling).dropna()
+
+        data5 = grouped_data.resample('5min', on='date').agg(sampling).dropna()
+        data15 = data5.reset_index().groupby('symbol').resample(
+            '15min', on='date').agg(sampling).dropna()
+        data60 = data15.reset_index().groupby("symbol").resample(
+            '60min', on='date').agg(sampling).dropna()
+        data1d = data60.reset_index().groupby('symbol').resample(
+            '1d', on='date').agg(sampling).dropna()
+
+        resampled_data = {
+            "iData3min": data3.reset_index(),
+            "iData5min": data5.reset_index(),
+            "iData15min": data15.reset_index(),
+            "iData60min": data60.reset_index(),
+            "iData1day": data1d.reset_index(),
+        }
+
+        resample_time = time.time() - resampling_start
+
+        summary_table = Table(
+            box=box.SQUARE, title="Resampling Summary", header_style="bold cyan")
+        summary_table.add_column("Interval", justify="center")
+        summary_table.add_column("Records", justify="right")
+        for k, v in resampled_data.items():
+            summary_table.add_row(k, str(len(v)))
+        console.print(summary_table)
+
+        console.print(
+            f"[green]Resampling completed in {resample_time:.2f}s[/green]")
+        return resampled_data
+
+
+def api_request(api, instrument_list, from_date, to_date, interval):
+    req_start = time.time()
+    request_count = 0
+    complete_data = pd.DataFrame()
+
+    with Progress(
+        "[progress.description]{task.description}",
+        "[progress.percentage]{task.percentage:>3.0f}%",
+        "•",
+        "{task.completed}/{task.total}",
+        "•",
+        "[cyan]{task.fields[rate]}[/cyan]",
+        transient=False,
+    ) as progress:
+        task = progress.add_task("Downloading", total=len(
+            instrument_list), rate="0.00 req/s")
+
+        for index, instrument in instrument_list.iterrows():
+            try:
+                data = api.historical_data(
+                    instrument["instrument_token"],
+                    from_date=from_date,
+                    to_date=to_date,
+                    interval=interval,
+                )
+
+                request_count += 1
+                if len(data) != 0:
+                    data = pd.DataFrame(data)
+                    data['symbol'] = instrument['name'] + '-I'
+                    data['open'] = data['open'].astype(float).round(2)
+                    data['high'] = data['high'].astype(float).round(2)
+                    data['low'] = data['low'].astype(float).round(2)
+                    data['close'] = data['close'].astype(float).round(2)
+                    data['volume'] = data['volume'].astype(int)
+                    complete_data = pd.concat(
+                        [complete_data, data], ignore_index=True)
+                else:
+                    time.sleep(0.01)  # slight delay for empty data
+            except Exception as e:
+                console.print(
+                    f"[red]Error downloading {instrument['tradingsymbol']}:[/red] {e}")
+
+            elapsed = time.time() - req_start
+            rate = f"{request_count / elapsed:.2f} req/s"
+            progress.update(task, advance=1, rate=rate)
+
+    console.print(
+        f"[green]Download completed for {len(instrument_list)} instruments.[/green]")
+    console.print(f"Time taken: {time.time() - req_start:.2f}s")
+    return complete_data
+
+
+def print_info(exchange, segment, instrument_list, interval, current_expiry, previous_expiry, from_date, to_date, no_of_days, profile):
+    table_info = Table(box=box.SIMPLE_HEAVY)
+    table_info.title = f"[bold magenta]Historical Data Fetcher"
+    table_info.add_column("User Id", style="cyan", justify="right")
+    table_info.add_column("Exchange", style="cyan", justify="right")
+    table_info.add_column("Segment", style="bold white")
+    table_info.add_column("Total Instruments", style="bold white")
+    table_info.add_column("Interval", style="bold white")
+
+    table_info.add_row(
+        profile["user_id"],
+        exchange.upper(),
+        segment,
+        str(len(instrument_list)),
+        interval,
+    )
+
+    console.print(Align.center(table_info))
+
+    table_info = Table(box=box.SIMPLE_HEAVY)
+    table_info.title = "[bold magenta]Expiry and Date Range Info[/bold magenta]"
+    table_info.add_column("Current Expiry", style="bold white")
+    table_info.add_column("Previous Expiry", style="bold white")
+    table_info.add_column("From Date", style="bold white")
+    table_info.add_column("To Date", style="bold white")
+    table_info.add_column("No. of Days", style="bold white")
+    table_info.add_row(
+        str(current_expiry.date()),
+        str(previous_expiry.date()),
+        str(from_date),
+        str(to_date),
+        str(no_of_days)
+    )
+
+    console.print(Align.center(table_info))
+
+
+def instruments(exchange='nfo'):
+    expiry_dates = pd.read_csv("instruments/expiries.csv")
+    expiry_dates.sort_values(by="expiry", inplace=True)
+    expiry_dates["expiry"] = pd.to_datetime(expiry_dates["expiry"])
+
+    current_expiry = expiry_dates[expiry_dates["expiry"] >= pd.to_datetime(
+        datetime.now().date())].iloc[0]["expiry"]
+    previous_expiry = expiry_dates[expiry_dates["expiry"] < pd.to_datetime(
+        datetime.now().date())].iloc[-1]["expiry"]
+
+    # if instruments-{exchange}.csv is not todays date then need to dowload todays from https://api.kite.trade/instruments
+    if not os.path.exists(f"instruments/instruments-{exchange}.csv") or \
+            pd.to_datetime(os.path.getmtime(f"instruments/instruments-{exchange}.csv"), unit='s').date() != datetime.now().date():
+        console.print(
+            f"[bold yellow]Instruments file for {exchange} is outdated or missing. Downloading...[/bold yellow]")
+        # Download the instruments file
+        url = f"https://api.kite.trade/instruments"
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(f"instruments/instruments-{exchange}.csv", "wb") as f:
+                f.write(response.content)
+        else:
+            console.print(
+                f"[bold red]Failed to download instruments file for {exchange}[/bold red]")
+            return None, None, None
+
+    instrument_list = pd.read_csv(f"instruments/instruments-{exchange}.csv")
+    instrument_list['expiry'] = pd.to_datetime(
+        instrument_list['expiry'], errors='coerce')
+    instrument_list = instrument_list[instrument_list['exchange'] == exchange.upper(
+    )]
+    instrument_list = instrument_list[instrument_list['segment'] == 'NFO-FUT']
+
+    instrument_list = instrument_list[instrument_list['expiry'] == pd.to_datetime(
+        current_expiry)]
+    instrument_list.sort_values(by="tradingsymbol", inplace=True)
+    instrument_list.reset_index(drop=True, inplace=True)
+
+    return instrument_list, current_expiry, previous_expiry
