83a0864bbc9e186a8b6a97be31fd11cea7ac2833
Saravana Kumar <saravanakumar@Saravanas-MacBook-Air.local>
2025-10-29 20:25:54 +0530

    update - 2025-10-28_14:59

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,8 +10,6 @@ function App() {
   const api = import.meta.env.VITE_API_URL;
   const provider = { api };
 
-  console.log("API URL:", api);
-
   return (
     <Router>
       <GlobalContext.Provider value={provider}>
